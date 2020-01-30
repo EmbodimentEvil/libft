9ed40294d616418c9c6de4c6f55d0f4dbfb3823d
EmbodimentEvil <46787637+EmbodimentEvil@users.noreply.github.com>
2020-01-30 15:27:14 +0300

    Fixed atof

--- source/string/ft_atof.c
+++ source/string/ft_atof.c
@@ -12,31 +12,74 @@
 
 #include "ft_string.h"
 
+static ssize_t	ft_len_nbr(int nbr)
+{
+	ssize_t		len;
+
+	len = 0;
+	while (nbr > 0)
+	{
+		nbr /= 10;
+		++len;
+	}
+	return (len);
+}
+
+static double	calc_mantissa(const char *str, int i)
+{
+	ssize_t		mantissa;
+
+	mantissa = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		mantissa = mantissa * 10 + (str[i] - '0');
+		i++;
+	}
+	return (mantissa / (double)pow(10, ft_len_nbr(mantissa)));
+}
+
+static int		check_sign(int sign, char symb)
+{
+	if (symb == '-' && sign == 0)
+		sign = -1;
+	else if (symb == '-' && sign == -1)
+		return (0);
+	else if (symb == '-' && sign == 1)
+		return (0);
+	else if (symb == '+' && sign == 0)
+		sign = 1;
+	else if (symb == '+' && sign == -1)
+		return (0);
+	else if (symb == '+' && sign == -1)
+		return (0);
+	return (sign);
+}
+
 double			ft_atof(const char *str)
 {
-	double	n;
-	int		i;
-	int		k;
-	int		signe;
+	int			i;
+	int			sign;
+	double		nbr;
 
-	i = 0;
-	n = 0;
-	k = 0;
-	signe = 1;
-	if (!str)
-		return (0);
-	while (str[i] == ' ' || str[i] == '\v' || str[i] == '\t' || str[i] == '\r'
-			|| str[i] == '\f' || str[i] == '\n')
-		i++;
-	signe = (str[i] == '-') ? -signe : signe;
-	i = (str[i] == '-' || str[i] == '+') ? i + 1 : i;
-	while (str[i] > 47 && str[i] < 58)
+	i = -1;
+	sign = 0;
+	nbr = 0;
+	while (str[++i])
 	{
-		n = n * 10 + (str[i++] - 48);
-		if (str[i] == '.')
-			k = i++;
+		if (str[i] == '.' || (str[i] >= '0' && str[i] <= '9'))
+			break ;
+		else if ((sign = check_sign(sign, str[i])) == 0)
+			return (nbr);
 	}
-	while (k != 0 && str[++k])
-		signe = signe * 10;
-	return (n / signe);
+	if (sign == 0)
+		sign = 1;
+	while (str[i] == '.' || (str[i] >= '0' && str[i] <= '9'))
+	{
+		if (str[i] == '.')
+			return (sign * (nbr + calc_mantissa(str, i + 1)));
+		nbr = nbr * 10 + (str[i] - 48);
+		i++;
+	}
+	return (nbr * sign);
 }
+
